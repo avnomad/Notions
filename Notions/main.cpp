7b3795c57070168768110a7a2399cb3420ee6360
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-01-31 00:45:55 +0200

    Removed the 'intThrower' hack to allow program to play under windows 7 or later.
    Corrected some #include directives.
    Excluded library source files from build.
    
    History note:
    Back in 2009, it is possible that I forked Mark III to make Mark III thread and
    then continued Mark III. That would explain why the includes and DIMouse members
    in Mark III thread looked older.
    
    --HG--
    branch : Mark III Thread

--- Notions/main.cpp
+++ Notions/main.cpp
@@ -5,12 +5,12 @@
 
 // includes
 #include "global.h"
-#include "../../unsorted/windows common.h"
-#include "../../unsorted/DirectInput Wrapper.h"
-#include "../../unsorted/Keyboard Wrapper.h"
-#include "../../unsorted/Mouse Wrapper.h"
-#include "../../unsorted/CPU clock.h"
-#include "../../unsorted/StopWatch.h"
+#include <windows/common.h>
+#include <Direct Input/DirectInput Wrapper.h>
+#include <Direct Input/Keyboard Wrapper.h>
+#include <Direct Input/Mouse Wrapper.h>
+#include <CPU clock.h>
+#include <StopWatch.h>
 #include <boost/thread.hpp>
 
 // prototypes.
@@ -22,19 +22,12 @@ void keyPressSpecial(int key , int x , int y);
 void mouseClick(int button, int state, int x, int y);
 
 
-ofstream output("c:/output/test.txt");
+ofstream output("test.txt");
 StopWatch gTimer("ms");
 unsigned int gN;
+StopWatch timer("ms");
 
 
-void intTrower()
-{
-	throw 0;
-} // end function intThrower
-
-void displayLoop()
-{
-} // end function displayLoop
 
 
 void deviceLoop(InstanceHandle currentInstance , WindowHandle mainWindow)
@@ -47,8 +40,8 @@ void deviceLoop(InstanceHandle currentInstance , WindowHandle mainWindow)
 	CHAR oldButton1 = 0;
 
 	DirectInput directInput(currentInstance);
-	DIKeyboard keyboard(directInput.iObject,mainWindow);
-	DIMouse mouse(directInput.iObject,mainWindow);
+	DIKeyboard keyboard(directInput,mainWindow);
+	DIMouse mouse(directInput,mainWindow);
 
 	keyboard.acquire();
 	mouse.acquire();
@@ -62,38 +55,38 @@ void deviceLoop(InstanceHandle currentInstance , WindowHandle mainWindow)
 
 		
 		if((keyboard.key[DIK_RETURN] & 0x80) && !(oldKey[DIK_RETURN] & 0x80))
-			keyPress('\r',mouse.absX,mouse.absY);
+			keyPress('\r',mouse.absolute.x,mouse.absolute.y);
 		if((keyboard.key[DIK_BACK] & 0x80) && !(oldKey[DIK_BACK] & 0x80))
-			keyPress('\b',mouse.absX,mouse.absY);
+			keyPress('\b',mouse.absolute.x,mouse.absolute.y);
 		if((keyboard.key[DIK_ESCAPE] & 0x80) && !(oldKey[DIK_ESCAPE] & 0x80))
 		{
-			keyPress('\33',mouse.absX,mouse.absY);
+			keyPress('\33',mouse.absolute.x,mouse.absolute.y);
 			break;
 		} // end if
 
 		if((keyboard.key[DIK_RIGHT] & 0x80) && !(oldKey[DIK_RIGHT] & 0x80))
-			keyPressSpecial(GLUT_KEY_RIGHT,mouse.absX,mouse.absY);
+			keyPressSpecial(GLUT_KEY_RIGHT,mouse.absolute.x,mouse.absolute.y);
 		if((keyboard.key[DIK_LEFT] & 0x80) && !(oldKey[DIK_LEFT] & 0x80))
-			keyPressSpecial(GLUT_KEY_LEFT,mouse.absX,mouse.absY);
+			keyPressSpecial(GLUT_KEY_LEFT,mouse.absolute.x,mouse.absolute.y);
 		if((keyboard.key[DIK_UP] & 0x80) && !(oldKey[DIK_UP] & 0x80))
-			keyPressSpecial(GLUT_KEY_UP,mouse.absX,mouse.absY);
+			keyPressSpecial(GLUT_KEY_UP,mouse.absolute.x,mouse.absolute.y);
 		if((keyboard.key[DIK_DOWN] & 0x80) && !(oldKey[DIK_DOWN] & 0x80))
-			keyPressSpecial(GLUT_KEY_DOWN,mouse.absX,mouse.absY);
+			keyPressSpecial(GLUT_KEY_DOWN,mouse.absolute.x,mouse.absolute.y);
 
 		memcpy(oldKey,keyboard.key,sizeof(oldKey));
 
 		if((mouse.button[0] & 0x80) && !(oldButton0 & 0x80))				// left button changed state
-			mouseClick(GLUT_LEFT_BUTTON,GLUT_DOWN,mouse.absX,mouse.absY);
+			mouseClick(GLUT_LEFT_BUTTON,GLUT_DOWN,mouse.absolute.x,mouse.absolute.y);
 		else if(!(mouse.button[0] & 0x80) && (oldButton0 & 0x80))
-			mouseClick(GLUT_LEFT_BUTTON,GLUT_UP,mouse.absX,mouse.absY);
+			mouseClick(GLUT_LEFT_BUTTON,GLUT_UP,mouse.absolute.x,mouse.absolute.y);
 
 		if((mouse.button[1] & 0x80) && !(oldButton1 & 0x80))				// right button changed state
-			mouseClick(GLUT_RIGHT_BUTTON,GLUT_DOWN,mouse.absX,mouse.absY);
+			mouseClick(GLUT_RIGHT_BUTTON,GLUT_DOWN,mouse.absolute.x,mouse.absolute.y);
 		else if(!(mouse.button[1] & 0x80) && (oldButton1 & 0x80))
-			mouseClick(GLUT_RIGHT_BUTTON,GLUT_UP,mouse.absX,mouse.absY);
+			mouseClick(GLUT_RIGHT_BUTTON,GLUT_UP,mouse.absolute.x,mouse.absolute.y);
 
 		if((mouse.button[0] & 0x80) && (oldButton0 & 0x80) && (mouse.X || mouse.Y))
-			drag(mouse.absX,mouse.absY);
+			drag(mouse.absolute.x,mouse.absolute.y);
 
 
 		oldButton0 = mouse.button[0];
@@ -103,15 +96,43 @@ void deviceLoop(InstanceHandle currentInstance , WindowHandle mainWindow)
 	output << setw(15) << timer.pop()/n << timer.getUnit() << endl;
 	output << setw(15) << gTimer.pop()/gN << gTimer.getUnit() << endl;
 	output.close();
-	system("start c:/output/test.txt");
+	system("start test.txt");
 	exit(0);
 } // end function deviceLoop
 
 
+InstanceHandle thisInstance;
+
+void always()
+{
+	++gN;
+	timer.pop();
+	timer.push();
+	paint();			
+}
+
+void initialize()
+{
+	// the mouse coordinates will be mapped correctly to window coordinates only in full-screen anyway...
+	windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
+	windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+	glViewport(0,0,windowWidth,windowHeight);
+
+	WindowHandle mainWindow = FindWindow(0,"Free-Curve to Line convertion");
+
+	boost::thread events(deviceLoop,thisInstance,mainWindow);
+
+	gTimer.push();
+	gN = 0;
+	timer.push();
+
+	glutIdleFunc(always);
+}
 
 // main
 int WINAPI WinMain(InstanceHandle currentInstance , InstanceHandle PreviusInstance , CHAR *commandLineArguments , int windowMode)
 {
+	// glut initialization
 	int argc = 1;
 	char *dummy = "Mark III";
 	char **argv = &dummy;
@@ -120,46 +141,20 @@ int WINAPI WinMain(InstanceHandle currentInstance , InstanceHandle PreviusInstan
 	glutInitWindowSize(windowWidth,windowHeight);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutCreateWindow("Free-Curve to Line convertion");
+	glutFullScreen();
 
 	LARGE_INTEGER freq;
 	QueryPerformanceFrequency(&freq);
 	multiplier = 1.0e6 / freq.QuadPart;
 
-	glutFullScreen();
+	thisInstance = currentInstance;
+
+	// OpenGL initialization
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0,1,0,1);
-	glutDisplayFunc(intTrower);
-	try
-	{
-		glutMainLoop();
-	}
-	catch(int)
-	{
-		// do nothing
-	} // end catch
 
-	WindowHandle mainWindow = FindWindow(0,"Free-Curve to Line convertion");
-
-	boost::thread events(deviceLoop,currentInstance,mainWindow);
-
-	StopWatch timer("ms");
-
-	gTimer.push();
-	gN = 0;
-	timer.push();
-	while(1)
-	{
-		//if(timer.peek() > 12)
-		//{
-			++gN;
-			timer.pop();
-			timer.push();
-			paint();			
-		//} // end if
-	} // end while
-
-
-
-
+	// main loop
+	glutIdleFunc(initialize);
+	glutMainLoop();
 	return 0;
 } // end function WinMain
