43001c498b0fc3d5d8d41fd4e03e287c40f3d5a4
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-05-10 22:38:55 +0300

    Added copyright notices and license.
    --HG--
    branch : Notions

--- Notions/global variables.h
+++ Notions/global variables.h
@@ -1,3 +1,21 @@
+//	Copyright (C) 2009, 2011, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Notions.
+ *
+ *	Notions is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Notions is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Notions.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef GLOBAL_VARIABLES_H
 #define GLOBAL_VARIABLES_H
 
